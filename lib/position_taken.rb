2c9a095114cecd09168007646cea02e1a2de200e
Robert Cobb <rwcobbjr@gmail.com>
2019-02-17 17:52:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,8 +4,7 @@
 # index is a number to check 
 # it could be 0, 4, 7, 1
 def position_taken?(board, index)
-  position = board[index]
-  if position == "" || position == " " || position == nil
+  if board[index] == "" || board[index] == " " || board[index] == nil
     return false 
   else
     return true
